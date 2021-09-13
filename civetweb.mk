91c0fed3d69426c63994a78b1a0cd6232e70f32b
Sophie Luna Schumann <me@luna.vg>
2021-09-13 10:21:30 +0000

    switch to cmake

--- civetweb.mk
+++ civetweb.mk
@@ -7,10 +7,12 @@ $(PKG)_IGNORE   		 :=
 $(PKG)_VERSION  		 := 1.15
 $(PKG)_CHECKSUM 		 := 90a533422944ab327a4fbb9969f0845d0dba05354f9cacce3a5005fa59f593b9
 $(PKG)_GH_CONF  		 := civetweb/civetweb/tags,v
-$(PKG)_DEPS			     := zlib
+$(PKG)_DEPS			     := zlib openssl
 #$(PKG)_DEPS_$(BUILD) := cmake
 
 define $(PKG)_BUILD
-		cd '$(1)' && \
-			$(MAKE) -C '$(1)' lib install-lib install-headers PREFIX='$(PREFIX)/$(TARGET)' WITH_IPV6=1 WITH_ZLIB=1 WITH_CPP=1 COPT="-DNDEBUG -DNO_CGI -DNO_SSL -DUSE_ZLIB" -j $(JOBS)
+		cd '$(BUILD_DIR)' && \
+			$(TARGET)-cmake '$(SOURCE_DIR)' -DCIVETWEB_BUILD_TESTING=OFF -DCIVETWEB_ENABLE_CXX=ON -DCIVETWEB_ENABLE_IPV6=ON -DCIVETWEB_DISABLE_CGI=ON -DCIVETWEB_ENABLE_ZLIB=ON -DCIVETWEB_ENABLE_SSL=ON -DCMAKE_INSTALL_PREFIX='$(PREFIX)/$(TARGET)' -D_WIN32_WINNT=0x0600 -DWINSOCK_LIBRARIES='$(PREFIX)/$(TARGET)/lib/libws2_32.a' && \
+			$(TARGET)-cmake --build . --parallel '$(JOBS)' && \
+			$(TARGET)-cmake --install .
 endef
